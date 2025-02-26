77f58962a860f9958cb08479c8c6174b32544466
heryfirman <herriifirmansyah9999@gmail.com>
2025-02-26 16:49:19 +0700

    setup functional products

---
+++ backend/prisma/migrations/20250222105818_add_unit_to_product_and_category/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "unit" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "unit" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -62,4 +62,108 @@ app.patch('/note/edit/:id', async (req: Request, res: Response): Promise<void> =
     res.send(result);
 });
 
+
+app.get('/category', async (req: Request, res: Response) => {
+    const categories = await prisma.category.findMany();
+    res.json(categories);
+});
+
+app.post('/category/create', async (req: Request, res: Response): Promise<void> => {
+    const { name, unit } = req.body;
+
+    if (!name || !unit) {
+        res.status(400).send({ error: "Category name is required! " })
+        return;
+    }
+
+    try {
+        const newCategory = await prisma.category.create({
+            data: { name, unit },
+        });
+        res.status(201).send(newCategory);
+    } catch (error) {
+        res.status(500).send({ error: "An error occured while createing the category." })
+    }
+});
+
+app.patch('/category/edit/:id', async (req: Request, res: Response): Promise<void> => {
+    const id = req.params.id;
+    const { name, unit } = req.body;
+
+    if (!name || !unit) {
+        res.status(400).send({ error: "Category not avaiable!" })
+        return;
+    }
+
+    try {
+        const result = await prisma.category.update({
+            where: { id },
+            data: { name, unit },
+        });
+    
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({ error: "An error occured while edit the category." })
+    }
+});
+
+app.get('/products', async (req: Request, res: Response) => {
+    const products = await prisma.product.findMany();
+    res.json(products);
+});
+
+app.post("/product/create", async (req: Request, res: Response): Promise<void> => {
+    const { name, price, unit, categoryId } = req.body;
+
+    if (!name || !price || !unit || !categoryId) {
+        res.status(400).send({
+            error: "Nama, harga, unit, dan ID kategori diperlukan!",
+        });
+        return;
+    }
+
+    try {
+        const newProduct = await prisma.product.create({
+            data: {
+                name,
+                price: parseFloat(price),
+                unit,
+                categoryId,
+            },
+        });
+
+        res.status(201).send(newProduct);
+    } catch (error) {
+        res.status(500).send({ error: "Terjadi kesalahan saat membuat produk.", details: error });
+    }
+});
+
+app.patch('/product/edit/:id', async (req: Request, res: Response): Promise<void> => {
+    const id: string = req.params.id;
+    const { name, price, unit, categoryId } = req.body;
+
+    if (!name || !price || !unit || !categoryId) {
+        res.status(400).send({ error: "product not avaiable!" })
+        return;
+    }
+
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
+    if (!uuidRegex.test(id)) {
+        res.status(400).json({ error: "Invalid product ID format!" });
+        return;
+    }
+
+    try {
+        const result = await prisma.product.update({
+            where: { id },
+            data: { name, price, unit },
+        });
+    
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({ error: "An error occured while edit the product." })
+    }
+});
+
 app.listen(port, () => console.log(`Server run on http://localhost:${port}`));
